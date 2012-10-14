bd0f85601838705dc6551cbd7d362de2ce09adf4
Mihughes <mihughes@mymail.mines.edu>
2012-10-13 19:19:29 -0600

    back at master

--- TheMotivator.java
+++ TheMotivator.java
@@ -5,6 +5,8 @@ public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
 			System.out.println("You're awesome");
+		else if (score == 65)
+			System.out.println("duh, added to master");
 		else if (score > 90)
 			System.out.println("That's great");
 		else if (score > 60)
